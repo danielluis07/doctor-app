e6f1171375dcea42fa37b411f48368716defa4e3
danielluis07 <bombdaniel@hotmail.com>
2024-11-06 08:35:30 -0300

    middleware and auth changes

--- auth.ts
+++ auth.ts
@@ -107,5 +107,19 @@ export const {
   },
   adapter: DrizzleAdapter(db),
   session: { strategy: "jwt" },
+  cookies: {
+    sessionToken: {
+      name:
+        process.env.NODE_ENV === "production"
+          ? "__Secure-next-auth.session-token"
+          : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
   ...authConfig,
 });
